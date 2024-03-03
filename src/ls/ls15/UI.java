992a69e49a822bdca11d879e511daa65838d0e43
Gresha_11111_GT <0042570g@gmail.com>
2024-03-03 21:57:22 +0200

    ccccccсccccccc

--- src/ls/ls15/UI.java
+++ src/ls/ls15/UI.java
@@ -11,54 +11,78 @@ public class UI extends JFrame implements ActionListener {
 
     private JLabel text = new JLabel("0");
     UI(){
-        setSize(700,500);
-        setLayout(new FlowLayout());
+        text.setBounds(100, 0, 100, 100);
+
+        setSize(600,600);
+        setLayout(null);
         JPanel botElements = new JPanel();
 
+        JButton b0 = new JButton("0");
+        b0.setBounds(100,400 ,100 ,100);
+        add(b0);
+
         JButton b1 = new JButton("1");
-        b1.setPreferredSize(new Dimension(50, 50));
-        botElements.add(b1);
+        b1.setBounds(100,100 ,100 ,100);
+        add(b1);
 
         JButton b2 = new JButton("2");
-        botElements.add(b2);
+        b2.setBounds(200,100 ,100 ,100);
+        add(b2);
 
         JButton b3 = new JButton("3");
-        botElements.add(b3);
+        b3.setBounds(300,100 ,100 ,100);
+        add(b3);
 
         JButton b4 = new JButton("4");
+        b4.setBounds(100,200 ,100 ,100);
         add(b4);
 
         JButton b5 = new JButton("5");
+        b5.setBounds(200,200 ,100 ,100);
         add(b5);
 
         JButton b6 = new JButton("6");
+        b6.setBounds(300,200 ,100 ,100);
         add(b6);
 
         JButton b7 = new JButton("7");
+        b7.setBounds(100,300 ,100 ,100);
         add(b7);
 
         JButton b8 = new JButton("8");
+        b8.setBounds(200,300 ,100 ,100);
         add(b8);
 
         JButton b9 = new JButton("9");
+        b9.setBounds(300,300 ,100 ,100);
         add(b9);
 
         JButton b10 = new JButton("+");
+        b10.setBounds(400,300 ,100 ,100);
         add(b10);
 
         JButton b11 = new JButton("*");
+        b11.setBounds(400,400 ,100 ,100);
         add(b11);
 
         JButton b12 = new JButton("÷");
+        b12.setBounds(300,400 ,100 ,100);
         add(b12);
 
         JButton b13 = new JButton("=");
+        b13.setBounds(400,100 ,100 ,100);
         add(b13);
 
         JButton b14 = new JButton("⌦");
+        b14.setBounds(400,200 ,100 ,100);
         add(b14);
+
+        JButton b15 = new JButton("-");
+        b15.setBounds(200,400 ,100 ,100);
+        add(b15);
         add(botElements);
 
+        b0.addActionListener(this);
         b1.addActionListener(this);
         b2.addActionListener(this);
         b3.addActionListener(this);
@@ -113,6 +137,9 @@ public class UI extends JFrame implements ActionListener {
                 break;
             case "9":
                 text.setText(text.getText()+"9");
+                break;
+            case "0":
+                text.setText(text.getText()+"0");
                 break;
             case "⌦":
                 text.setText("0");
